714e9766b4767f22840bcd72eedc84d105177c52
bk7312 <bk7312@users.noreply.github.com>
2023-05-31 17:45:29 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,17 +1,8 @@
 
-// workaround for env not working
-const process = {
-    env: {
-        REACT_APP_ENDPOINT:'https://cloud.appwrite.io/v1',
-        REACT_APP_PROJECT_ID:'646ad7e4167a46151411',
-        REACT_APP_COLLECTION_ID:'646d7bce583874b898e8',
-        REACT_APP_DATABASE_ID:'646d77ba120f10283ceb',
-    }
-}
-
+// workaround for env not working, 
 export const Server = {
-    endpoint : process.env.REACT_APP_ENDPOINT,
-    project: process.env.REACT_APP_PROJECT_ID,
-    collectionID : process.env.REACT_APP_COLLECTION_ID,
-    databaseID : process.env.REACT_APP_DATABASE_ID,
+    endpoint : 'https://cloud.appwrite.io/v1',
+    project: '646ad7e4167a46151411',
+    // collectionID : process.env.REACT_APP_COLLECTION_ID,
+    // databaseID : process.env.REACT_APP_DATABASE_ID,
 }
