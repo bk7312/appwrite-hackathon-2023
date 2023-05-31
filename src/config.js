faa82c606b6a9f309690af37772bf667262a827b
bk7312 <bk7312@users.noreply.github.com>
2023-05-31 17:28:51 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
-import * as dotenv from 'dotenv'
 
-dotenv.config()
-
+// workaround for env not working
+const process = {
+    env: {
+        REACT_APP_ENDPOINT:'https://cloud.appwrite.io/v1',
+        REACT_APP_PROJECT_ID:'646ad7e4167a46151411',
+        REACT_APP_COLLECTION_ID:'646d7bce583874b898e8',
+        REACT_APP_DATABASE_ID:'646d77ba120f10283ceb',
+    }
+}
 
 export const Server = {
     endpoint : process.env.REACT_APP_ENDPOINT,
